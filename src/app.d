1f15d3c78735ee0b4e9d894281545d4537ee29df
Dmitry Ponyatov <dponyatov@gmail.com>
2023-12-23 22:24:48 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/app.d
+++ src/app.d
@@ -1,23 +1,66 @@
 import std.stdio;
 import std.string;
 
-import sdl;
+import bindbc.sdl;
 
-// class Window {
-//     SDL_Window* win = null;
-//     SDL_Renderer* render = null;
+class Window {
 
-//     this(string title) {
-//         win = SDL_CreateWindow(title.toStringz, SDL_WINDOWPOS_UNDEFINED,
-//                 SDL_WINDOWPOS_UNDEFINED, 320, 240, SDL_WINDOW_SHOWN);
-//         assert(win !is null);
-//         render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-//         assert(render !is null);
-//     }
+    enum W = 480;//240;
+    enum H = 640;//320;
 
-// }
+    SDL_Window* win = null;
+    SDL_Renderer* render = null;
+
+    this(string title) {
+        if (win is null)
+            assert(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) == 0);
+        win = SDL_CreateWindow(title.toStringz, SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED, W, H, SDL_WINDOW_SHOWN);
+        assert(win !is null);
+        render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+        assert(render !is null);
+    }
+
+    ~this() {
+        SDL_DestroyRenderer(render);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+    }
+
+    void clear() {
+        SDL_SetRenderDrawColor(render, 0x22, 0x22, 0x22, 0);
+        SDL_RenderClear(render);
+    }
+
+    void blit() {
+        clear;
+        // SDL_RenderCopy(render, yuv, null, &fs);
+        SDL_RenderPresent(render);
+    }
+
+    bool loop() {
+        blit;
+        SDL_Delay(222);
+        SDL_Event event;
+        SDL_PumpEvents;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+            case SDL_KEYDOWN:
+            case SDL_MOUSEBUTTONDOWN:
+                return false;
+            default:
+            }
+        }
+        return true;
+    }
+}
 
 void main(string[] args) {
     writeln(args);
-    assert(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) == 0);
+
+    auto win = new Window(args[0]);
+    while (win.loop()) {
+    }
+
 }
